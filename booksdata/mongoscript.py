1154d21479d77d15d07f822173118bd54c37b055
krkshitiz <93022866+krkshitiz@users.noreply.github.com>
2024-01-20 02:22:35 +0530

    Add files via upload

---
+++ booksdata/mongoscript.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+import datetime
+
+client = MongoClient(mongodb+srv://temp:123@scraping-scrapy-mongo.t1kbf8e.mongodb.net/)
+
+db = client.scrapy
+
+posts = db.test_collection
+
+post = {
+    "author": "Mike",
+    "text": "My first blog post!",
+    "tags": ["mongodb", "python", "pymongo"],
+    "date": datetime.datetime.now(tz=datetime.timezone.utc),
+}
+
+post_id = posts.insert_one(post).inserted_id
+
+print(post_id)
